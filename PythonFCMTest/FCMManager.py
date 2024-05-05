3776111d6d22cbd2ba19eeeb85594d31abe4e8db
MalakAlKazem <123973743+MalakAlKazem@users.noreply.github.com>
2024-05-05 22:33:45 +0300

    Update FCMManager.py

--- PythonFCMTest/FCMManager.py
+++ PythonFCMTest/FCMManager.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, messaging
 cred = credentials.Certificate("python-fcm-test-c2685-firebase-adminsdk-1gz7v-0f0c6d3bae.json")
 
-# cred = credentials.Certificate("C:\Users\USER\OneDrive\Desktop\PythonFCMTest\python-fcm-test-c2685-firebase-adminsdk-1gz7v-0f0c6d3bae.json")
 firebase_admin.initialize_app(cred)
 
 def sendPush(title, msg, registration_token, dataObject=None):
