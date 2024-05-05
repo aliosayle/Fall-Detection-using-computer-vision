92afe4e0b34db658886ab6af4b34cda18dd332be
MalakAlKazem <123973743+MalakAlKazem@users.noreply.github.com>
2024-05-05 22:32:40 +0300

    Update def sendPush in FCMManager.py

--- PythonFCMTest/FCMManager.py
+++ PythonFCMTest/FCMManager.py
@@ -6,7 +6,9 @@ cred = credentials.Certificate("python-fcm-test-c2685-firebase-adminsdk-1gz7v-0f
 firebase_admin.initialize_app(cred)
 
 def sendPush(title, msg, registration_token, dataObject=None):
-
+    dataObject = {
+        "key1": "value1",
+    }
     message = messaging.MulticastMessage(
         notification = messaging.Notification(
             title=title,
