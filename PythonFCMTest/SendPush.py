1bd50886b10c6985bb936f567df177751902a61d
MalakAlKazem <123973743+MalakAlKazem@users.noreply.github.com>
2024-05-05 22:30:59 +0300

    Update tokens in SendPush.py

--- PythonFCMTest/SendPush.py
+++ PythonFCMTest/SendPush.py
@@ -1,3 +1,3 @@
 import FCMManager as fcm
-tokens=["f17oqJgwS1qb7t3G6oNF7S:APA91bHj5phk3-pqbEUT6AHpkLtn_CGvdCOdAY83-c2o-vb70kiWfDvQnnV6c4R7_gGhKa5mu8p-FI-lQSyW_xE50Wk2Bvmp69vhuwQyOkAsz3kdXCSZ5zgIS1Ecy9skV0jbaaLWLT6m"]
+tokens=["d-PNMBUzQSin9FfJw4zFpY:APA91bHebukoi1uGDaJd7NQB9tqxxy6pwhRgosRUmjhTO0CDJuy7tyv9R3vVzsTzf1MkwFPs-XqcP7aSGcgg-UG4wKFS8cd75AhEabf93knsU2ZOINb0A22-h82HpD9k8pEEzBGrurgi"]
 fcm.sendPush("\U0001F4F8 Fall 490 ", "Fall has been detected, Please check your app now",tokens)
