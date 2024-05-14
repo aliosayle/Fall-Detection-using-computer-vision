789829ef5c097b0360db5fce81013e6e17a3b7c7
aliosseili <aliossaily24@gmail.com>
2024-05-14 22:31:38 +0300

    added application setup for threshold, cooldown and verbose

--- PythonFCMTest/test.py
+++ PythonFCMTest/test.py
@@ -10,8 +10,11 @@ CLIENT = InferenceHTTPClient(
 )
 now = datetime.datetime.now()
 count = 0
-last_minute = now.minute
+threshold = 5 #this changes how many detection per mintute will result in notification being sent
+cooldown = 60 #this changes how much time between each notification being sent
+last_minute = now.minute 
 last_call_time = 0
+verbose = 1
 
 cap = cv2.VideoCapture(0)
 
@@ -22,7 +25,7 @@ def getResult(frame):
 
 
 #this function will only return false if called more than once in the cooldown (default is 60s)
-def send_notification_with_cooldown(cooldown_time = 60):
+def send_notification_with_cooldown(cooldown_time = cooldown):
   global last_call_time
   current_time = time.time()
   if current_time - last_call_time >= cooldown_time:
@@ -32,7 +35,7 @@ def send_notification_with_cooldown(cooldown_time = 60):
     return False
 
 
-#this function returns True if it was called 5 times within a minute
+#this function returns True if it was called (threshold) times within a minute
 def call():
   global now, count, last_minute
   now = datetime.datetime.now()
@@ -44,7 +47,7 @@ def call():
     last_minute = now.minute
     print("counter reset")
   #this checks if more than 5 instances of fall were detected, and if no notifications were send in the last cooldown period
-  if count >= 5 and send_notification_with_cooldown():
+  if count >= threshold and send_notification_with_cooldown():
     return True
   else:
     return False
@@ -88,8 +91,8 @@ def video():
 
 # Ask user for input
 while True:
-  choice = input("Enter 1 for webcam or 2 for video: ")
-  if choice in ('1', '2'):
+  choice = input("Enter 1 for webcam or 2 for video or 3 for setup: ")
+  if choice in ('1', '2', '3'):
     break
   else:
     print("Enter a valid number")
@@ -98,5 +101,17 @@ while True:
 if choice == '1':
   while True:
     webcam()
-else:
+elif choice == '2':
   video()
+else:
+  print("setup mode")
+  threshold = int(input("enter the threshold, or how many detections per minute will result in a notification: "))
+  coolddown = int(input("enter the cooldown for notifications in seconds(default is 60): "))
+  verbose = int(input("verbose(1,2 or 3): "))
+  print("setup complete")
+  choice = input("choose 1 for webcam or 2 for test vid: ")
+  if choice == 1:
+    while True:
+      webcam()
+  else:
+    video()
