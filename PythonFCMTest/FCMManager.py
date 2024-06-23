37a9fcb4da409f70e78e4b3cfb2bff3ea3ac0c49
MalakAlKazem <123973743+MalakAlKazem@users.noreply.github.com>
2024-06-23 10:42:08 +0300

    Replace firebase project credentials

--- PythonFCMTest/FCMManager.py
+++ PythonFCMTest/FCMManager.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
-cred = credentials.Certificate("python-fcm-test-c2685-firebase-adminsdk-1gz7v-0f0c6d3bae.json")
-
+cred = credentials.Certificate("")//replace your firebase credentials 
 firebase_admin.initialize_app(cred)
 
 def sendPush(title, msg, registration_token, dataObject=None):
