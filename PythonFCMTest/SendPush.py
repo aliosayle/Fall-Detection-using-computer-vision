036e019f540b85e0f8c557976c8d81d7c68b0a62
aliosayle <108302752+aliosayle@users.noreply.github.com>
2024-06-23 10:53:33 +0300

    Update SendPush.py
    removed api key

--- PythonFCMTest/SendPush.py
+++ PythonFCMTest/SendPush.py
@@ -2,7 +2,7 @@ import FCMManager as fcm
 
 
 def main():
-  tokens=["d-PNMBUzQSin9FfJw4zFpY:APA91bHebukoi1uGDaJd7NQB9tqxxy6pwhRgosRUmjhTO0CDJuy7tyv9R3vVzsTzf1MkwFPs-XqcP7aSGcgg-UG4wKFS8cd75AhEabf93knsU2ZOINb0A22-h82HpD9k8pEEzBGrurgi"]
+  tokens=[""] #replace this with you actual key from firebase
   fcm.sendPush("\U0001F4F8 Fall 490 ", "Fall has been detected, Please check your app now",tokens)
 
 if __name__ == "__main__":
